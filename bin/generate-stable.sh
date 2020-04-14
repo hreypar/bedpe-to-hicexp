6c08d2cb19589040374cf34f5915224953fdf844
hreypar <hreypar@gmail.com>
2020-04-14 18:43:10 -0500

    generate sample column

--- bin/generate-stable.sh
+++ bin/generate-stable.sh
@@ -6,7 +6,8 @@ stable_full_path=$2
 find -L $stable_directory \
 	-type f \
 	-name "*.bedpe.Rds" \
-| sort \
+	-exec sh \
+		-c 'echo -n $0,; basename $0 | sed -r "s#-rep.+##"' {} \; \
 > $stable_full_path
 
 
