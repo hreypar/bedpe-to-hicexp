843984145f02543059f1626dae527d36e0718dc5
hreypar <hreypar@gmail.com>
2020-04-14 18:58:35 -0500

    add read in data code

--- bin/bedpe-to-hicexp.R
+++ bin/bedpe-to-hicexp.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 #
-# hreyes Feb 2020
+# hreyes Apr 2020
 # bedpe-to-hicexp.R
 #
 # Read in bedpe files (with replicates) and generate a hicexp object
@@ -10,40 +10,56 @@
 library(optparse)
 library(dplyr)
 library(multiHiCcompare)
+library(GenomicRanges)
 #library(magrittr)
 #library(purrr)
 #
 options(scipen = 10)
 #
 ########################## read in data ###################################
-
-# receive STABLE AND THE OTHER make_hicexp ARGUMENTS
-# READ IN THE STABLE AS A DATA FRAME AND USE THE COLUMNS TO FILL HIC DATA AND GROUPS
-#
-# read RDS files into a list
-#provide the list to make_hicexp
-# does make_hicexp understand the data list names as the groups?
-
 option_list = list(
-  make_option(opt_str = c("-s", "--samples"), 
+  make_option(opt_str = c("-s", "--stable"), 
               type = "character",
-              default = NULL, 
-              help = "A table where the first column is a bedpe file path and the second is its sample name")
+              default = "data/breast-cancer-1mb/breast-cancer-1mb.stable", 
+              help = "Input stable: plain text file where the first column specifies a .bedpe.Rds file route and the second column specifies a sample group"),
+  make_option(opt_str = c("-z", "--zeroprop"), 
+              type = "numeric", 
+              default = 0.8, 
+              help = "If the proportion of zeroes in a row is <= z the row is filtered out. Default is 0.8"),
+  make_option(opt_str = c("-a", "--average"),
+              type = "numeric", 
+              default = 5, # should it actually be a percentage of the total reads? 
+              help = "Minimum average value (for a row). Default is 5"),
+  make_option(opt_str = c("-r", "--removeregions"), 
+              type = "character", 
+              default = "hg38_cyto", 
+              help = "GenomicRanges object indicating specific regions to be filtered out. 
+              Default is hg38 centromeric, gvar and stalk regions. Set to NULL if you know what you're doing"),
+  make_option(opt_str = c("-o", "--output"),
+              type = "character", 
+              default = "results/breast-cancer-1mb/breast-cancer-1mb.hicexp.Rds", 
+              help = "Output Rds file: hicexp object with more than two Hi-C matrices")
 )
-#
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
+if (is.null(opt$stable)){
+  print_help(OptionParser(option_list=option_list))
+  stop("The stable input file is mandatory.n", call.=FALSE)
+}
+
+input.stable <- read.csv(file = opt$stable, header = FALSE, stringsAsFactors = FALSE)
+
+hic.matrices.list <- lapply(input.stable[,1], readRDS)
+hic.matrices.sample <- input.stable[,2]
 #
 ######################### build hicexp object #############################
-outhicexp <- make_hicexp(b1, b2, b3, b4, 
-                       groups = c(0, 0, 1, 1), 
-                       zero.p = 0.8, A.min = 5, filter = TRUE,
-                       remove.regions = hg38_cyto)
+outhicexp <- make_hicexp(data_list = hic.matrices.list,
+                         groups = hic.matrices.sample,
+                         zero.p = opt$zeroprop,
+                         A.min = opt$average,
+                         filter = TRUE,
+                         remove.regions = opt$removeregions)
 
-hicexp_40kb_NC2 <- make_hicexp(hic_raw_40kb$`MCF-10A-rep1`, hic_raw_40kb$`MCF-10A-rep2`,
-                               hic_raw_40kb$`MCF-7-rep1`, hic_raw_40kb$`MCF-7-rep2`,
-                               groups = c(1,1,2,2),
-                               zero.p = 0.8, A.min = 5, filter = TRUE,
-                               remove.regions = hg38_cyto)
-
-  
-# then you save the object with a unique name within R and that's that
+# save hicexp as Rds
 save(list = outhicexp, file = out_hicexp_path)
